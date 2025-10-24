82c34640dbcb69983ded670df904305f1feb4a18
Essmann <kenwilliam1000@gmail.com>
2025-10-24 12:55:42 +0200

    Oppg3 fullført.

--- src/main/java/no/hvl/dat100/oppgave3/Blogg.java
+++ src/main/java/no/hvl/dat100/oppgave3/Blogg.java
@@ -5,68 +5,109 @@ import no.hvl.dat100.oppgave1.*;
 
 public class Blogg {
 
-	// TODO: objektvariable 
+    // TODO: objektvariable
+    Innlegg[] innlegg;
+    int nesteLedig;
 
-	public Blogg() {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
-	}
+    public Blogg() {
+        this.innlegg = new Innlegg[20];
+        this.nesteLedig = 0;
+    }
 
-	public Blogg(int lengde) {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
-	}
+    public Blogg(int lengde) {
+        this.innlegg = new Innlegg[lengde];
+        this.nesteLedig = 0;
+    }
 
-	public int getAntall() {
-		throw new UnsupportedOperationException(TODO.method());
-	}
-	
-	public Innlegg[] getSamling() {
-		throw new UnsupportedOperationException(TODO.method());
+    public int getAntall() {
+        return this.nesteLedig;
+    }
 
-	}
-	
-	public int finnInnlegg(Innlegg innlegg) {
+    public Innlegg[] getSamling() {
+        return this.innlegg;
 
-		throw new UnsupportedOperationException(TODO.method());
-	}
+    }
 
-	public boolean finnes(Innlegg innlegg) {
-		throw new UnsupportedOperationException(TODO.method());
-	}
+    public int finnInnlegg(Innlegg innlegg) {
+        int index = 0;
+        for (Innlegg x : this.innlegg) {
 
-	public boolean ledigPlass() {
-		throw new UnsupportedOperationException(TODO.method());
+            if (x != null && x.erLik(innlegg)) {
+                return index;
+            }
+            index++;
+        }
+        return -1;
+    }
 
-	}
-	
-	public boolean leggTil(Innlegg innlegg) {
+    public boolean finnes(Innlegg innlegg) {
+        return finnInnlegg(innlegg) != -1;
 
-		throw new UnsupportedOperationException(TODO.method());
-	}
-	
-	public String toString() {
-		throw new UnsupportedOperationException(TODO.method());
-	}
+    }
 
-	// valgfrie oppgaver nedenfor
-	
-	public void utvid() {
-		throw new UnsupportedOperationException(TODO.method());
-	}
-	
-	public boolean leggTilUtvid(Innlegg innlegg) {
+    public boolean ledigPlass() {
+        return nesteLedig < this.innlegg.length;
 
-		throw new UnsupportedOperationException(TODO.method());
-		
-	}
-	
-	public boolean slett(Innlegg innlegg) {
-		
-		throw new UnsupportedOperationException(TODO.method());
-	}
-	
-	public int[] search(String keyword) {
-		
-		throw new UnsupportedOperationException(TODO.method());
+    }
 
-	}
+    public boolean leggTil(Innlegg innlegg) {
+        if (!finnes(innlegg)) {
+            this.innlegg[nesteLedig] = innlegg;
+            nesteLedig++;
+            return true;
+        }
+        return false;
+    }
+
+    public String toString() {
+        int antall = getAntall();
+        String string = antall + "\n";
+        for (Innlegg innlegg : getSamling()) {
+            if (innlegg != null) {
+                string += innlegg.toString();
+            }
+        }
+        return string;
+    }
+
+    // valgfrie oppgaver nedenfor
+
+    public void utvid() {
+        Innlegg[] gammel = this.innlegg;
+        this.nesteLedig = 0;
+        Innlegg[] ny = new Innlegg[gammel.length * 2];
+        for (int i = 0; i < gammel.length; i++) {
+            ny[i] = gammel[i];
+            nesteLedig++;
+        }
+        this.innlegg = ny;
+
+    }
+
+    public boolean leggTilUtvid(Innlegg innlegg) {
+
+        throw new UnsupportedOperationException(TODO.method());
+
+    }
+
+    public boolean slett(Innlegg innlegg) {
+        int target_index = finnInnlegg(innlegg);
+        if (target_index != -1) {
+            // shift all elements after target_index one position to the left
+            for (int i = target_index; i < nesteLedig - 1; i++) {
+                this.innlegg[i] = this.innlegg[i + 1];
+            }
+            // remove duplicate last element
+            this.innlegg[nesteLedig - 1] = null;
+            nesteLedig--;
+            return true;
+        }
+        return false;
+    }
+
+    public int[] search(String keyword) {
+
+        throw new UnsupportedOperationException(TODO.method());
+
+    }
 }
