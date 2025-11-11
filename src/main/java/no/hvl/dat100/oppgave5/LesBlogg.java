151d54c4f966352b6cdf7995b1f49afdcaa1bae8
Essmann <kenwilliam1000@gmail.com>
2025-11-11 16:00:46 +0100

    Oppg5 fullført.

--- src/main/java/no/hvl/dat100/oppgave5/LesBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave5/LesBlogg.java
@@ -6,7 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Scanner;
+import java.util.*;
 
 import no.hvl.dat100.common.TODO;
 import no.hvl.dat100.oppgave1.*;
@@ -14,15 +14,66 @@ import no.hvl.dat100.oppgave2.*;
 import no.hvl.dat100.oppgave3.*;
 
 import javax.swing.JOptionPane;
+import javax.swing.text.Keymap;
 
 public class LesBlogg {
 
-	private static String TEKST = "TEKST";
-	private static String BILDE = "BILDE";
+    private static String TEKST = "TEKST";
+    private static String BILDE = "BILDE";
 
-	public static Blogg les(String mappe, String filnavn) {
+    private static Object dictToClass(List<String> list) {
+        int id = Integer.parseInt(list.get(1));
+        String brukernavn = list.get(2);
+        String dato = list.get(3);
+        int likes = Integer.parseInt(list.get(4));
+        String tekst = list.get(5);
+        if (list.get(0).equals(BILDE)) {
+            String url = list.get(6);
+            return new Bilde(id, brukernavn, dato, likes, tekst, url);
+        }
+        return new Tekst(id, brukernavn, dato, likes, tekst);
 
-		throw new UnsupportedOperationException(TODO.method());
 
-	}
+    }
+
+    public static Blogg les(String mappe, String filnavn) {
+        String filePath = mappe + filnavn;
+        File file = new File(filePath);
+        Blogg blogg = null;
+
+        //
+        List<String> list = new ArrayList<>();
+
+
+
+        try (Scanner reader = new Scanner(file)) {
+            //Finn lengden på blogg.
+            int antall = Integer.parseInt(reader.nextLine());
+            blogg = new Blogg(antall);
+
+
+
+            while (reader.hasNextLine()) {
+                String data = reader.nextLine();
+                if (data.equals(TEKST) || data.equals(BILDE)) {
+                    if (!list.isEmpty()) {
+                        //method call
+                        blogg.leggTil((Innlegg) dictToClass(list));
+                        list.clear();
+                    }
+
+
+                }
+                list.add(data);
+
+
+            }
+            if (!list.isEmpty()) {
+                blogg.leggTil((Innlegg) dictToClass(list));
+            }
+        } catch (Exception e) {
+        }
+        return blogg;
+    }
+
 }
