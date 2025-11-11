6df78326c3b172527b5200fa95db36568870d905
Essmann <kenwilliam1000@gmail.com>
2025-11-11 14:25:35 +0100

    Oppg4 fullført.

--- src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
@@ -1,7 +1,9 @@
 package no.hvl.dat100.oppgave4;
 
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.io.Writer;
 
 import no.hvl.dat100.common.TODO;
 import no.hvl.dat100.oppgave3.*;
@@ -10,6 +12,18 @@ public class SkrivBlogg {
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
 
-		throw new UnsupportedOperationException(TODO.method());
+        String filePath = mappe + filnavn;
+        System.out.println(filePath);
+        FileWriter writer;
+        try{
+            writer = new FileWriter(filePath);
+            writer.append(samling.toString());
+            writer.flush();
+            return true;
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        return false;
 	}
 }
